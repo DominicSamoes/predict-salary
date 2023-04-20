e7c2d6733f227aa7429e33962a6e9deb6c470e45
DominicSamoes <dominicsamo@gmail.com>
2023-04-20 15:59:38 +0200

    Prediction working

--- src/Predict.jsx
+++ src/Predict.jsx
@@ -1,21 +1,47 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./style.css";
 
 const Predict = () => {
+  const [prediction, setPrediction] = useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios
+      .post("http://localhost:5000/predict_api", { experience, test_score, interview_score })
+      .then(response => {
+        console.log(response)
+        setPrediction(response.data)
+      })
+  }
 
   const handleReload = () => {
     window.location.href = "/";
   }
 
+  const [experience, setExperience] = useState()
+  const [test_score, setTestScore] = useState()
+  const [interview_score, setInterviewScore] = useState()
+
   return (
     <section className="container">
     <h1>Predict Salary</h1>
-    <form action="" method="post">
+    <form method="post" onSubmit={handleSubmit}>
      <fieldset>
        <legend className="visually-hidden">Predict Salary</legend>
        <div className="form-group col-1-2">
          <div className="form-field">
            <span className="form-field-container">
-             <input type="text" name="experience" placeholder="Number of years worked" pattern="[0-9]|[1-4][0-9]|50" maxLength="2" required={true} />
+             <input 
+              type="text" 
+              name="experience"
+              value={experience} 
+              placeholder="Number of years worked" 
+              pattern="[0-9]|[1-4][0-9]|50" 
+              maxLength="2" 
+              required={true} 
+              onChange={e => setExperience(e.target.value)} 
+            />
              <i className="form-field-icon"></i>
            </span>
            <p className="form-help">Experience should be between 0 and 50</p>
@@ -24,7 +50,16 @@ const Predict = () => {
        <div className="form-group col-1-2">
          <div className="form-field">
            <span className="form-field-container">
-             <input type="text" name="text_score" placeholder="Test Score" pattern="[0-9]|10" maxLength="2"  required={true} />
+             <input 
+              type="text" 
+              name="test_score" 
+              value={test_score}
+              placeholder="Test Score" 
+              pattern="[0-9]|10" 
+              maxLength="2"  
+              required={true} 
+              onChange={e => setTestScore(e.target.value)}
+            />
              <i className="form-field-icon"></i>
            </span>
            <p className="form-help">Test score should be between 0 and 10</p>
@@ -33,19 +68,28 @@ const Predict = () => {
        <div className="form-group col-1-2">
          <div className="form-field">
            <span className="form-field-container">
-            <input type="text" name="interview_score" placeholder="Interview Score" pattern="[0-9]|10" maxLength="2"  required={true} />
+            <input
+             type="text" 
+             name="interview_score" 
+             value={interview_score}
+             placeholder="Interview Score" 
+             pattern="[0-9]|10" maxLength="2"  
+             required={true} 
+             onChange={e => setInterviewScore(e.target.value)}
+            />
              <i className="form-field-icon"></i>
            </span>
            <p className="form-help">Interview score should be between 0 and 10</p>
          </div>
        </div>
        <br />
-       <button type="submit" className="button">Predict</button>
+       <button 
+        type="submit" className="button">Predict</button>
        <br />
        <button type="reset" className="button" onClick={handleReload}>Clear</button>
      </fieldset>
     </form>
-    <p className="form-help">{/* Prediction Text Will be here */}</p>
+    <p className="form-help">{prediction}</p>
   </section> 
   );
 };
